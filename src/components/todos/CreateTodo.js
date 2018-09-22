f0637c51607cbf6b99ee59e25b5a78f9eb313bba
Maverick Tesini <mavericktesini@gmail.com>
2018-09-22 01:04:08 +0000

    automatically backed up by learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,68 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
-constructor() {
-  super();
-  this.state = {
-    text: '',
-  };
-}
-
-handleChange(event) {
-  this.setState({
-    text: event.target.value
-  });
-};
-
+ 
 class CreateTodo extends Component {
-  render() {
-    return(
-      <div>
-        <form>
-          <p>
-            <label>add todo</label>
-            <input type="text" onChange={(event) => this.handleChange(event)}/>
-          </p>
-          <input type="submit" />
-        </form>
-        {this.state.text}
-      </div>
-    );
+ 
+  state = {
+    text: ''
   }
-};
-
-export default CreateTodo;
-import React, { Component } from 'react';
-
-class CreateTodo extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      text: '',
-    };
-  }
-
+ 
   handleChange = event => {
     this.setState({
       text: event.target.value
     });
   }
-
+ 
   handleSubmit = event => {
-    event.preventDefault();
+    event.preventDefault()
     this.props.addTodo(this.state)
   }
-
+ 
   render() {
     return(
       <div>
-        <form onSubmit={ event => this.handleSubmit(event) }>
+        <form onSubmit={ (event) => this.handleSubmit(event) }>
           <p>
             <label>add todo</label>
             <input
           type="text"
-          onChange={(event) => this.handleChange(event)} value={this.state.text}/>
+          onChange={ (event) => this.handleChange(event) } value={ this.state.text }/>
           </p>
           <input type="submit" />
        </form>
@@ -70,15 +34,11 @@ class CreateTodo extends Component {
    );
   }
 };
-
-
+ 
 mapDispatchToProps = dispatch => {
   return {
-    // In terms of action, we could write out a separate actions file and import it in,
-    // but for now, we'll just write in an action to get a clearer idea of how this is working:
-    addTodo: formData => dispatch( {type: 'ADD_TODO', payload: formData} )
+    addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
   }
 }
-// Since we only need to dispatch an action here and not getting information from our store,
-// we can use null instead of mapStateToProps as the first argument.
+ 
 export default connect(null, mapDispatchToProps)(CreateTodo);
