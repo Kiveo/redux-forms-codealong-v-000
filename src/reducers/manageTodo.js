0791e5cab8ad371310bf385241e003be7a7487cd
Maverick Tesini <mavericktesini@gmail.com>
2018-09-22 01:09:16 +0000

    automatically backed up by learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -5,7 +5,7 @@ export default function manageTodo(state = {todos: [], action}) {
   switch (action.type) {
     case 'ADD_TODO':
 
-      console.log({ todos: state.todos.concat(action.payload.text) });
+      // console.log({ todos: state.todos.concat(action.payload.text) });
 
       return { todos: state.todos.concat(action.payload.text) };
 
